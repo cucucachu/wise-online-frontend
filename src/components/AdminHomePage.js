52a5341e930b6b22112cab57263aa572991bb31c
cucucachu <codymjones@ymail.com>
2020-09-10 10:13:00 -0700

    Admin Home Page

---
+++ src/components/AdminHomePage.js
@@ -0,0 +1,33 @@
+import React　from 'react';
+import { Link } from 'react-router-dom'
+
+import attendanceIcon from '../Assets/images/attendance-icon.png';
+
+
+const AdminHomePage = (props) => {
+
+    return ( 
+        <div className="container">
+                <img src={ attendanceIcon } className="page-icon" alt="login icon"/>
+                <div className="spacer-vertical-s"></div>
+                <h1>{sessionStorage.getItem('schoolName')}</h1>
+                <div className="spacer-vertical"></div>
+
+                <div className="spacer-vertical-s"></div>
+
+                <Link to="/admin-terms">
+                       
+                    <button className="btn-l">
+                    Academic Terms
+                    </button>
+                </Link>
+                
+                <div className="spacer-vertical-s"></div>
+                <Link to="/set-up-school">
+                    <button className="btn-l">Add Students and Professors</button>
+                </Link>
+        </div>
+    );
+  }
+   
+  export default AdminHomePage;
