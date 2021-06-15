881d2d24f2651f904d69bc017834e9ab64104d25
Gregory Arnheiter <gmarnheiter@gmail.com>
2021-06-15 15:04:45 -0700

    Translations added for studentFeeWaiveForm

--- src/components/studentFeeWaiveForm.js
+++ src/components/studentFeeWaiveForm.js
@@ -75,27 +75,27 @@ class StudentFeeWaiveForm extends Component {
         <div className="container prevent-text">
             <img src={educationIcon} className="page-icon" alt="wise education icon"/>
             <div className="spacer-vertical" />
-            <h1>Input your information</h1>
+            <h1>{i18n("Input your information")}</h1>
             <form onSubmit={this.handleSubmit.bind(this)}>
             <div className="spacer-vertical" />
                 <div className="input-wrapper">
                     <div style={this.state.showHide}>{this.state.message}</div>
-                    <span className="input-label">First Name</span>
-                    <input id="student_first_name" type="text" placeholder="First Name" className="" value={this.state.firstName} onChange={this.handleFirstName.bind(this)} onFocus={() => this.onFocus("student_first_name")} required/>
+                    <span className="input-label">{i18n("First Name")}</span>
+                    <input id="student_first_name" type="text" placeholder={i18n("First Name")} className="" value={this.state.firstName} onChange={this.handleFirstName.bind(this)} onFocus={() => this.onFocus("student_first_name")} required/>
                 </div>
                 <div className="spacer-vertical-s" />
                 <div className="input-wrapper">
-                    <span className="input-label">Last Name</span>
-                    <input id="student_last_name" type="text" placeholder="Last Name" className="" value={this.state.lastName} onChange={this.handleLastName.bind(this)} onFocus={() => this.onFocus("student_last_name")} required/>
+                    <span className="input-label">{i18n("Last Name")}</span>
+                    <input id="student_last_name" type="text" placeholder={i18n("Last Name")} className="" value={this.state.lastName} onChange={this.handleLastName.bind(this)} onFocus={() => this.onFocus("student_last_name")} required/>
                 </div>
                 <div className="spacer-vertical-s" />
                 <div className="input-wrapper">
-                    <span className="input-label">Email</span>
-                    <input id="student_email" type="email" placeholder="Email" className="" onChange={this.handleEmail.bind(this)} value={this.state.email} onFocus={() => this.onFocus("student_email")} required/>
+                    <span className="input-label">{i18n("Email")}</span>
+                    <input id="student_email" type="email" placeholder={i18n("Email")} className="" onChange={this.handleEmail.bind(this)} value={this.state.email} onFocus={() => this.onFocus("student_email")} required/>
                 </div>
                 <div className="spacer-vertical" />
                 <div className="">
-                    <input id="student_submit" type="submit" className="btn" value="Next" onFocus={() => this.onFocus("student_submit")} />
+                    <input id="student_submit" type="submit" className="btn" value={i18n("Next")} onFocus={() => this.onFocus("student_submit")} />
                 </div>
             </form>
     </div>
