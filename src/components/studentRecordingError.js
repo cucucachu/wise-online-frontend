9584da4d4ce6aa1e5a62e366c589b6169ed19f66
Gregory Arnheiter <gmarnheiter@gmail.com>
2021-06-15 14:37:20 -0700

    Translations added for studentRecordingError

--- src/components/studentRecordingError.js
+++ src/components/studentRecordingError.js
@@ -9,28 +9,28 @@ const StudentRecError = (props) => {
             <div className="container">
                 <img src={editIcon} className="page-icon" alt="edit icon"/>
                 <div className="spacer-vertical" />
-                <h1>Recording error</h1>
+                <h1>{i18n("Recording error")}</h1>
                 <div className="spacer-vertical" />
                 <div className="width-adjust-1">
-                    <h2>How to allow camera permissions</h2>
-                    <p className="text-plain">Wise uses confidential facial recognition to make sure you are really you.</p>
+                    <h2>{i18n("How to allow camera permissions")}</h2>
+                    <p className="text-plain">{i18n("Wise uses confidential facial recognition to make sure you are really you.")}</p>
                     <div className="spacer-vertical-s"></div>
-                    <p className="text-black" style={{fontWeight: 'bold', fontSize: 'large'}}>How to activate by browser</p>
+                    <p className="text-black" style={{fontWeight: 'bold', fontSize: 'large'}}>{i18n("How to activate by browser")}</p>
                     <div className="text-plain">
-                        Chrome: <br/>
-                        Firefox:<br/>
-                        Safar:<br/>
-                        Microsoft Edge:<br/>
-                        Opera:<br/><br/>
-                        Email suppport: support@wiseattend.com<br/><br />
+                        {i18n("Chrome")}: <br/>
+                        {i18n("Firefox")}:<br/>
+                        {i18n("Safar")}:<br/>
+                        {i18n("Microsoft Edge")}:<br/>
+                        {i18n("Opera")}:<br/><br/>
+                        {i18n("Email suppport: support@wiseattend.com")}<br/><br />
                   
-                        <p className="text-black" style={{fontWeight: 'bold', fontSize: 'large'}}>Refresh the page when ready</p>
+                        <p className="text-black" style={{fontWeight: 'bold', fontSize: 'large'}}>{i18n("Refresh the page when ready")}</p>
                     </div>
       
                 </div>
                 <div className="spacer-vertical" />
                 <Link to="/student/test/record">
-                    <button className="btn">Refresh page</button>
+                    <button className="btn">{i18n("Refresh page")}</button>
                 </Link>
                 
             </div>
