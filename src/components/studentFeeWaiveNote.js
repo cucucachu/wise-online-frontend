d8a8dc897e7f440811f78a8049dc441bca6ff6f3
Gregory Arnheiter <gmarnheiter@gmail.com>
2021-06-15 14:59:32 -0700

    Translations added for studentFeeWaiveNote

--- src/components/studentFeeWaiveNote.js
+++ src/components/studentFeeWaiveNote.js
@@ -12,16 +12,16 @@ const StudentFeeWaiveNote = (props) => {
             <div className="container">
                 <img src={educationIcon} className="page-icon" alt="wise education icon"/>
                 <div className="spacer-vertical" />
-                <h1>Input your information</h1>
+                <h1>{i18n("Input your information")}</h1>
                 <div className="spacer-vertical" />
                 <div className="width-slim">
                    <p className="text-plain">
-                   Please enter the correct information associated with your school to ensure that your free-waiver is successful.
+                   {i18n("Please enter the correct information associated with your school to ensure that your free-waiver is successful.")}
                    </p>
                 </div>
                 <div className="spacer-vertical" />
                 <Link to="fee-waiver-form">
-                    <button className="btn">Next</button>
+                    <button className="btn">{i18n("Next")}</button>
                 </Link>
                 
             </div>
