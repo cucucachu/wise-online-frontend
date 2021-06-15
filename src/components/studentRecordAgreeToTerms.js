76d17b6062bd5bebb4bec3116c5708d4579dc131
Gregory Arnheiter <gmarnheiter@gmail.com>
2021-06-15 14:47:12 -0700

    Translations added for studentRecordAgreeToTerms

--- src/components/studentRecordAgreeToTerms.js
+++ src/components/studentRecordAgreeToTerms.js
@@ -28,34 +28,29 @@ const StudentRecordAgreeToTerms = (props) => {
             <div className="container">
                 <img src={editIcon} className="page-icon" alt="edit icon"/>
                 <div className="spacer-vertical" />
-                <h1>ATTENTION: YOU WILL BE RECORDED</h1>
+                <h1>{i18n("ATTENTION: YOU WILL BE RECORDED")}</h1>
                 <div className="spacer-vertical-s"></div>
                 
                 <div className="width-adjust-1">
-                    <h4 className="" style={{color: '#222'}}>PLEASE CAREFULLY READ THE FOLLWING:</h4>
+                    <h4 className="" style={{color: '#222'}}>{i18n("PLEASE CAREFULLY READ THE FOLLWING")}:</h4>
                     <div className="spacer-vertical-s"></div>
                     <p style={{color: '#444'}}>
-                    By clicking “begin” you acknowledge that you are aware that you will be recorded by Wise through 
-                    your device webcam, and the content of your web-browsing tabs will be recorded and displayed to 
-                    professors, in effort to offer a secure exam proctoring experience at educational institutions. 
-                    You hereby re-affirm that you have read, understand, and agree to our&nbsp; 
+                    {i18n('studentRecordAgreeToTerms_acknowledge')}&nbsp; 
                     <a 
                         href="https://www.wiseattend.com/privacy" 
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        privacy policy
-                    </a> and &nbsp; 
+                        {i18n("privacy policy")}
+                    </a> {i18n("and")} &nbsp; 
                     <a 
                         href="https://www.wiseattend.com/privacy" 
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        terms of service
+                        {i18n("terms of service")}
                     </a>. 
-                    Please know that all your information which is generated as part of using Wise is considered 
-                    to be in the public domain. Your information may be stored and transmitted at the discretion 
-                    of Wise to provide a secure exam proctoring experience.
+                    {i18n("studentRecordAgreeToTerms_information")}
                     </p>
                 </div>
                 <div className="spacer-vertical" />
@@ -64,17 +59,17 @@ const StudentRecordAgreeToTerms = (props) => {
                     <span style={hasAgreed? {paddingRight: '5px', paddingLeft: '5px', } : {paddingLeft: '12px', paddingRight: '12px'}} className="mimic-radio height-lower" onClick={handleRadio} >{hasAgreed ? <strong>&#10003;</strong> : ''}</span>
                             
                         <strong style={{color: '#444'}} className="font-terms">
-                            &nbsp;I agree to the &nbsp;
+                            &nbsp;{i18n("I agree to the")} &nbsp;
                             <a 
                                 href="https://www.wiseattend.com/privacy" 
                                 target="_blank"
                                 rel="noopener noreferrer"
-                            >terms of use</a>.
+                            >{i18n("terms of use")}</a>.
                         </strong>
         
                     <div className="spacer-vertical" />
                 </div>
-                <button onClick={handleConfirm} className="btn">Next</button>
+                <button onClick={handleConfirm} className="btn">{i18n("Next")}</button>
                 
             </div>
         </React.Fragment>
