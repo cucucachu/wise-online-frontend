3e77b9a77141a884e511b88ab2eab78e503aaade
Gregory Arnheiter <gmarnheiter@gmail.com>
2021-06-15 15:11:34 -0700

    Translations added for studentFeeWaive

--- src/components/studentFeeWaive.js
+++ src/components/studentFeeWaive.js
@@ -23,16 +23,16 @@ const StudentFeeWaive = (props) => {
             <div className="container">
                 <img src={educationIcon} className="page-icon" alt="wise education icon"/>
                 <div className="spacer-vertical" />
-                <h1>Register for a fee-waiver</h1>
+                <h1>{i18n("Register for a fee-waiver")}</h1>
                 <div className="spacer-vertical" />
                 <div className="width-slim">
                    <p className="text-plain">
-                   If you are a student with financial need, please complete this fee-waiver form to register you free waiver for the Wise Education student fee.
+                   {i18n("If you are a student with financial need, please complete this fee-waiver form to register you free waiver for the Wise Education student fee.")}
                    </p>
                 </div>
                 <div className="spacer-vertical" />
                 <Link to="fee-waiver-select-school">
-                    <button className="btn">Begin Registration</button>
+                    <button className="btn">{i18n("Begin Registration")}</button>
                 </Link>
                 
             </div>
