6690df530efc70512505fcaf8d566081d04502b1
Gregory Arnheiter <gmarnheiter@gmail.com>
2021-06-15 15:21:28 -0700

    Translations added for studentAttSuccess

--- src/components/studentAttSuccess.js
+++ src/components/studentAttSuccess.js
@@ -18,15 +18,15 @@ class StudentAttSuccess extends Component {
         <div className="container">
             <img src={attendClass} className="page-icon" alt="login icon"/>
             <div className="spacer-vertical" />
-                <h1 style={{color: '#4ebd0f'}}>Success!</h1>
+                <h1 style={{color: '#4ebd0f'}}>{i18n("Success!")}</h1>
 
             <div className="spacer-vertical-s"></div>
             <img src={successIcon} className="page-icon" alt="success icon" style={{boxShadow: '0 0 0 transparent'}}/>
             <div className="spacer-vertical-s"></div>
-            <p className="text-plain">Your attendance has been registered for:</p>
+            <p className="text-plain">{i18n("Your attendance has been registered for:")}</p>
             <h2>{this.state.classID}</h2>
             <Link to="/student/dashboard">
-                <button className="btn" >Done</button>
+                <button className="btn" >{i18n("Done")}</button>
             </Link>
             
     </div>
