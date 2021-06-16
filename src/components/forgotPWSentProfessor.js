bc61f7ca7e372b12bfd1df9fade23d96520f5523
Gregory Arnheiter <gmarnheiter@gmail.com>
2021-06-15 19:59:59 -0700

    Translations added for forgotPWSentProfessor

--- src/components/forgotPWSentProfessor.js
+++ src/components/forgotPWSentProfessor.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom"
 
 import loginIcon from '../Assets/images/login-icon.png'
 
+import { i18n } from 'web-translate';
+
 class ForgotPWSentProfessor extends Component {
     
   render(){
@@ -11,12 +13,10 @@ class ForgotPWSentProfessor extends Component {
         <div className="container">
             <img src={loginIcon} className="page-icon" alt="login icon"/>
             <div className="spacer-vertical" />
-            <h1>Forgot my Password</h1>
+            <h1>{i18n("Forgot my Password")}</h1>
             <div className="spacer-vertical-s"></div>
             <div className="width-adjust-1">
-                <p className="text-plain ">
-                Reset link sent! Please allow a few moments for the email to reach your inbox.
-                </p>
+                <p className="text-plain ">{i18n("Reset link sent! Please allow a few moments for the email to reach your inbox.")}</p>
             </div>
             <div className="spacer-vertical" />
  
@@ -24,7 +24,7 @@ class ForgotPWSentProfessor extends Component {
                 <div className="spacer-vertical" />
                 <div className="">
                     <Link to="/">
-                    <button className="btn">Back</button>
+                    <button className="btn">{i18n("Back")}</button>
                     </Link>
                 </div>
     </div>
