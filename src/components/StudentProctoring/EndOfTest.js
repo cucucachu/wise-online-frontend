ecb2f4bc650085fb6ea95b43913feef4d5fa20e7
Gregory Arnheiter <gmarnheiter@gmail.com>
2021-06-15 23:01:25 -0700

    Translations added for StudentProctoring EndOfTest

--- src/components/StudentProctoring/EndOfTest.js
+++ src/components/StudentProctoring/EndOfTest.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
+import { i18n } from 'web-translate';
+
 function EndOfTest(props) {
     return (
         <div className={`shadow center${props.show ? '' : ' display-none'}`}>
-            <h1>Test Completed</h1>
+            <h1>{i18n("Test Completed")}</h1>
             <div className="spacer-vertical-s"></div>
             <div className="width-80 center">
-                <p className="black">If you weren't finished, you can start the test again and it will continue where you left off.</p>
+                <p className="black">{i18n("If you weren't finished, you can start the test again and it will continue where you left off.")}</p>
                 <div className="spacer-vertical-s"></div>
                 <div>
-                    <button className='btn' onClick={props.toStudentDashboard}>Home</button>
+                    <button className='btn' onClick={props.toStudentDashboard}>{i18n("Home")}</button>
                 </div>
                 <div className="spacer-vertical-s"></div>
                 <div>
-                    <button className='btn' onClick={props.restart}>I wasn't finished!</button>
+                    <button className='btn' onClick={props.restart}>{i18n("I wasn't finished!")}</button>
                 </div>
             </div>
         </div>
