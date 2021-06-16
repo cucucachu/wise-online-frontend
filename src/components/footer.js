9b15ac1d1517d42564924e0042ecd93fcb427031
Gregory Arnheiter <gmarnheiter@gmail.com>
2021-06-15 20:05:25 -0700

    Translations added for footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 
+import { i18n } from 'web-translate';
+
 class Footer extends Component {
   render(){
       return(
           <footer>
               <p className="footer-style">
-              ®2020 Wise Education Systems, Inc.  All Rights Reserved
+              ®2020 Wise Education Systems, Inc.  {i18n("All Rights Reserved")}
               </p>
           </footer>
       )
