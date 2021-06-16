6efd88b9096f41e4f599c3b944e06e14174aad83
Gregory Arnheiter <gmarnheiter@gmail.com>
2021-06-15 22:36:27 -0700

    Translations added for adminNewTermCard

--- src/components/adminNewTermCard.js
+++ src/components/adminNewTermCard.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { i18n } from 'web-translate';
+
 class AdminNewTermCard extends Component {
     constructor(props) {
         super(props);
@@ -20,15 +22,15 @@ class AdminNewTermCard extends Component {
                         <div className="row">
                             <div className="col-sm-6">
                                 <form onSubmit={this.props.handleSubmit}>
-                                    <input type="text" placeholder="Enter term name" className="input-settings" name="name" onChange={this.props.handleChange}/>
+                                    <input type="text" placeholder={i18n("Enter term name")} className="input-settings" name="name" onChange={this.props.handleChange}/>
                                     {/* <input type="text" placeholder="Enter term ID" className="input-settings" onChange={this.props.handleChange} name="id"/> */}
                                     
-                                    <input type="submit" style={{textAlign: 'center'}} className="btn-upload" value="Create"/>
+                                    <input type="submit" style={{textAlign: 'center'}} className="btn-upload" value={i18n("Create")}/>
                                 </form>
                             </div>
                             <div className="col-sm-6 text-plain-s">
-                                Create a term name<br/>
-                                e.g. Wise Term 1
+                                {i18n("Create a term name")}<br/>
+                                {i18n("e.g. Wise Term 1")}
                             </div>
                         </div>
                     </div>
@@ -41,7 +43,7 @@ class AdminNewTermCard extends Component {
                     <div className="create-course-div shadow" >
                         <div className="row">
                             <div className="create-course">
-                                <h2 className="center-v-h">Create new term</h2>
+                                <h2 className="center-v-h">{i18n("Create new term")}</h2>
 
                             </div>
                         </div>
