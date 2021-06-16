5753ed142471f20ed3fa679ae25e3a15c0c2cfd8
Gregory Arnheiter <gmarnheiter@gmail.com>
2021-06-15 22:33:00 -0700

    Translations added for adminResetPWSuccess

--- src/components/adminResetPWSuccess.js
+++ src/components/adminResetPWSuccess.js
@@ -5,6 +5,8 @@ import successIcon from '../Assets/images/success-icon.png'
 
 import { Link } from 'react-router-dom';
 
+import { i18n } from 'web-translate';
+
 class AdminResetPWSuccess extends Component {
 
 
@@ -13,15 +15,15 @@ class AdminResetPWSuccess extends Component {
         <div className="container">
             <img src={loginIcon} className="page-icon" alt="login icon"/>
             <div className="spacer-vertical" />
-                <h1 style={{color: '#4ebd0f'}}>Success!</h1>
+                <h1 style={{color: '#4ebd0f'}}>{i18n("Success!")}</h1>
 
             <div className="spacer-vertical-s"></div>
             <img src={successIcon} className="page-icon" alt="success icon" style={{boxShadow: '0 0 0 transparent'}}/>
             <div className="spacer-vertical-s"></div>
-            <p className="text-plain">Your password has been reset.</p>
+            <p className="text-plain">{i18n("Your password has been reset.")}</p>
             <div className="spacer-vertical" />
             <Link to="/admin-login">
-                <button className="btn" >Log in</button>
+                <button className="btn" >{i18n("Log in")}</button>
             </Link>
             
     </div>
