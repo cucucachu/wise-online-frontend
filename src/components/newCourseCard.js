fa1dc141db95f389f9bbdcecbee353f9ee2a1714
Gregory Arnheiter <gmarnheiter@gmail.com>
2021-06-15 19:48:32 -0700

    Translations added for newCourseCard

--- src/components/newCourseCard.js
+++ src/components/newCourseCard.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { i18n } from 'web-translate';
+
 class NewCourseCard extends Component {
     constructor(props) {
         super(props);
@@ -30,7 +32,7 @@ class NewCourseCard extends Component {
                     <div className="shadow" >
                         <div className="row">
                             <div className="col-sm-10">
-                                <h2 className="course-title">New Course</h2>
+                                <h2 className="course-title">{i18n("New Course")}</h2>
                             </div>
                             <div className="col-sm-2">
                                 <button className="btn-neutral" onClick={this.handleClickCancel}><div style={{textAlign: 'center'}}>&#128473;</div></button>
@@ -39,16 +41,16 @@ class NewCourseCard extends Component {
                         <div className="row">
                             <div className="col-sm-6">
                                 <form onSubmit={this.props.handleSubmit}>
-                                    <input type="text" placeholder="Enter class name" style={this.props.inputStype} onChange={this.props.handleChangeName}/>
-                                    <input type="text" placeholder="Enter class ID" style={this.props.inputStype} onChange={this.props.handleChangeID}/>
-                                    <input type="submit" style={{textAlign: 'center'}} className="btn-upload" value="Create"/>
+                                    <input type="text" placeholder={i18n("Enter class name")} style={this.props.inputStype} onChange={this.props.handleChangeName}/>
+                                    <input type="text" placeholder={i18n("Enter class ID")} style={this.props.inputStype} onChange={this.props.handleChangeID}/>
+                                    <input type="submit" style={{textAlign: 'center'}} className="btn-upload" value={i18n("Create")}/>
                                 </form>
                             </div>
                             <div className="col-sm-6 text-plain-s">
-                                Create a class name<br/>
-                                e.g. ECON 101<br/>
-                                Create a unique class ID<br/>
-                                e.g. My ECON 101
+                                {i18n("Create a class name")}<br/>
+                                {i18n("e.g. ECON 101")}<br/>
+                                {i18n("Create a unique class ID")}<br/>
+                                {i18n("e.g. My ECON 101")}
                             </div>
                         </div>
                     </div>
@@ -61,7 +63,7 @@ class NewCourseCard extends Component {
                     <div className="create-course-div shadow" >
                         <div className="row">
                             <div className="create-course">
-                                <h2 className="center-v-h">Create course</h2>
+                                <h2 className="center-v-h">{i18n("Create course")}</h2>
 
                             </div>
                         </div>
