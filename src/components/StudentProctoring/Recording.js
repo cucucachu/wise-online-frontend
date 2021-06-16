28ffa33127846291357e65996b37be633b8d559e
Gregory Arnheiter <gmarnheiter@gmail.com>
2021-06-15 22:49:18 -0700

    Translations added for StudentProctoring Recording

--- src/components/StudentProctoring/Recording.js
+++ src/components/StudentProctoring/Recording.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { i18n } from 'web-translate';
+
 class GetPrivileges extends Component {
     constructor(props) {
         super(props);
@@ -25,18 +27,18 @@ class GetPrivileges extends Component {
     render() {
         return (
             <div className={`shadow center${this.props.show ? '' : ' display-none'}`}>
-                <h1>YOU ARE NOW BEING PROCTORED</h1>
+                <h1>{i18n("YOU ARE NOW BEING PROCTORED")}</h1>
                 <div className="spacer-vertical-s" />
                 <div className="width-80 center">
-                    <h2>Please open and complete your exam.</h2>
-                    <div className="text-large txt-thin">When you're done, click I'm done!</div>
+                    <h2>{i18n("Please open and complete your exam.")}</h2>
+                    <div className="text-large txt-thin">{i18n("When you're done, click I'm done!")}</div>
                     {(() => {
                         if (this.props.test)
                             return (
                                 <>
                                     <div className="spacer-vertical-s" />
                                     <h4 className="black">
-                                        Your test link is:&nbsp;
+                                        {i18n("Your test link is:")}&nbsp;
                                         <a
                                             href={this.state.link && this.state.link}
                                             target="_blank"
@@ -44,7 +46,7 @@ class GetPrivileges extends Component {
                                         >{this.props.test.testLink}</a>
                                     </h4>
                                     <h4 className="black">
-                                        Your test password is:&nbsp; {this.props.test.testPassword}
+                                        {i18n("Your test password is:")}&nbsp; {this.props.test.testPassword}
                                     </h4>
                                 </>
                             );
