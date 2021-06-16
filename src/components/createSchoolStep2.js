eda84d8d5dc730271e1a0310114846f223b66977
Gregory Arnheiter <gmarnheiter@gmail.com>
2021-06-15 20:11:02 -0700

    Translations added for createSchoolStep2

--- src/components/createSchoolStep2.js
+++ src/components/createSchoolStep2.js
@@ -15,33 +15,33 @@ class SchoolStep2 extends Component {
             <div className="container">
                     <img src={editIcon} className="page-icon" alt="login icon"/>
                     <div className="spacer-vertical" />
-            <h1>{i18n("HERE")}Create Your School</h1>
+            <h1>{i18n("Create Your School")}</h1>
 
             <form>
                 <div className="spacer-vertical" />
                 <div className="input-wrapper">
-                    <span className="input-label">{i18n("HERE")}Email</span>
+                    <span className="input-label">{i18n("Email")}</span>
                     <input type="email" className="" />
                 </div>
                 
                 <div className="spacer-vertical-s"></div>
                 <div className="input-wrapper">
-                    <span className="input-label">{i18n("HERE")}Confirm Email</span>
+                    <span className="input-label">{i18n("Confirm Email")}</span>
                     <input type="email" className="" />
                 </div>
                 <div className="spacer-vertical-s"></div>
                 <div className="input-wrapper">
-                    <div className="input-label">{i18n("HERE")}Password</div>
+                    <div className="input-label">{i18n("Password")}</div>
                     <input type="password" className="" />
                 </div>
                 <div className="spacer-vertical-s"></div>
                 <div className="input-wrapper">
-                    <span className="input-label">{i18n("HERE")}Confirm Password</span>
+                    <span className="input-label">{i18n("Confirm Password")}</span>
                     <input type="password" className="" />
                 </div>
                 <div className="spacer-vertical" />
                 <div className="">
-                    <Link to="/set-up-school"><button className="btn">{i18n("HERE")}Next</button></Link>
+                    <Link to="/set-up-school"><button className="btn">{i18n("Next")}</button></Link>
                 </div>
             </form>
             </div>
