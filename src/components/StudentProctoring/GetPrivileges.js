b7f46a9d09dcba11fb20eb7747422c28359c4a22
Gregory Arnheiter <gmarnheiter@gmail.com>
2021-06-15 23:00:25 -0700

    Translations added for StudentProctoring GetPrivileges

--- src/components/StudentProctoring/GetPrivileges.js
+++ src/components/StudentProctoring/GetPrivileges.js
@@ -4,6 +4,8 @@ import TextWithCheckbox from '../Resusable/TextWithCheckbox';
 
 import { proctoringVerifyPrivileges } from '../../store/axios';
 
+import { i18n } from 'web-translate';
+
 class GetPrivileges extends Component {
     constructor(props) {
         super(props);
@@ -74,22 +76,22 @@ class GetPrivileges extends Component {
             <div className={`shadow center${this.props.show ? '' : ' display-none'}`}>
                 <div className="spacer-vertical-s"></div>
                 <div className="width-80 center">
-                    <h2>Instructions</h2>
+                    <h2>{i18n("Instructions")}</h2>
                 <hr/>
                     <div className="spacer-vertical-s"></div>
                     <div className="black width-60 text-align-left">
                         <ul>
-                            <li>Your browser will request these permissions.</li>
-                            <li>If this doesn't happen automatically, click the checkboxes below to request the permissions.</li>
-                            <li>For screen share, you must select 'Your Entire Screen'.</li>
-                            <li>If you're having trouble, here are some helpful links.</li>
+                            <li>{i18n("Your browser will request these permissions.")}</li>
+                            <li>{i18n("If this doesn't happen automatically, click the checkboxes below to request the permissions.")}</li>
+                            <li>{i18n("For screen share, you must select 'Your Entire Screen'.")}</li>
+                            <li>{i18n("If you're having trouble, here are some helpful links.")}</li>
                             <ul>
                                 <li>
                                     <a 
                                         target="_blank"
                                         rel="noopener noreferrer"
                                         href="https://support.google.com/chrome/answer/2693767?co=GENIE.Platform%3DDesktop&hl=en"
-                                    >How to Enable Your Webcam</a>
+                                    >{i18n("How to Enable Your Webcam")}</a>
                                 </li>
                             </ul>
                         </ul>
@@ -97,25 +99,25 @@ class GetPrivileges extends Component {
                 </div>
                 <div className="spacer-vertical-s"></div>
                 <div className="width-80 center">
-                    <h2>Permissions</h2>
+                    <h2>{i18n("Permissions")}</h2>
                     <hr/>
                     {(() => { if (this.state.message) return <p className="red">{this.state.message}</p>; else return ''; })()}
                     <TextWithCheckbox 
-                        text="Webcam Permission"
+                        text={i18n("Webcam Permission")}
                         checked={this.props.webcamPrivilege}
                         onClick={this.handleClickEnableWebcam}
                         
                     />
                     {(() => { if (this.state.webcamMessage) return <p className="red">{this.state.webcamMessage}</p>; else return ''; })()}
                     <TextWithCheckbox 
-                        text="Screen Capture Permission"
+                        text={i18n("Screen Capture Permission")}
                         checked={this.props.screenshotPrivilege}
                         onClick={this.handleClickEnableScreenCapture}
                     />
                     {(() => { if (this.state.screenCaptureMessage) return <p className="red">{this.state.screenCaptureMessage}</p>; else return ''; })()}
                 </div>
                 <div className="spacer-vertical-s"></div>
-                <button onClick={this.handleContinue} className="btn">Continue</button>
+                <button onClick={this.handleContinue} className="btn">{i18n("Continue")}</button>
             </div>
         )
     }
