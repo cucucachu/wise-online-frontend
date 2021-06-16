428caa6250643848e6261fe943afc2759df68694
Gregory Arnheiter <gmarnheiter@gmail.com>
2021-06-15 20:10:04 -0700

    Translations added for createSchoolStep2

--- src/components/createSchoolStep2.js
+++ src/components/createSchoolStep2.js
@@ -5,6 +5,8 @@ import editIcon from '../Assets/images/edit-icon.png';
 
 import { AuthContext } from '../contexts/AuthContext'
 
+import { i18n } from 'web-translate';
+
 class SchoolStep2 extends Component {
     static contextType = AuthContext
   render(){
@@ -13,33 +15,33 @@ class SchoolStep2 extends Component {
             <div className="container">
                     <img src={editIcon} className="page-icon" alt="login icon"/>
                     <div className="spacer-vertical" />
-            <h1>Create Your School</h1>
+            <h1>{i18n("HERE")}Create Your School</h1>
 
             <form>
                 <div className="spacer-vertical" />
                 <div className="input-wrapper">
-                    <span className="input-label">Email</span>
+                    <span className="input-label">{i18n("HERE")}Email</span>
                     <input type="email" className="" />
                 </div>
                 
                 <div className="spacer-vertical-s"></div>
                 <div className="input-wrapper">
-                    <span className="input-label">Confirm Email</span>
+                    <span className="input-label">{i18n("HERE")}Confirm Email</span>
+                    <input type="email" className="" />
+                </div>
+                <div className="spacer-vertical-s"></div>
+                <div className="input-wrapper">
+                    <div className="input-label">{i18n("HERE")}Password</div>
                     <input type="password" className="" />
                 </div>
                 <div className="spacer-vertical-s"></div>
                 <div className="input-wrapper">
-                    <div className="input-label">Password</div>
-                    <input type="password" className="" />
-                </div>
-                <div className="spacer-vertical-s"></div>
-                <div className="input-wrapper">
-                    <span className="input-label">Confirm Password</span>
+                    <span className="input-label">{i18n("HERE")}Confirm Password</span>
                     <input type="password" className="" />
                 </div>
                 <div className="spacer-vertical" />
                 <div className="">
-                    <Link to="/set-up-school"><button className="btn">Next</button></Link>
+                    <Link to="/set-up-school"><button className="btn">{i18n("HERE")}Next</button></Link>
                 </div>
             </form>
             </div>
