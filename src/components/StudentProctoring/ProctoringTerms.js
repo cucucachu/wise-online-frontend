13685f05c35004d84be9ba5dadcc0b6a2719ddf7
Gregory Arnheiter <gmarnheiter@gmail.com>
2021-06-15 22:56:30 -0700

    Translations added for StudentProctoring ProctoringTerms

--- src/components/StudentProctoring/ProctoringTerms.js
+++ src/components/StudentProctoring/ProctoringTerms.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import TextWithCheckbox from '../Resusable/TextWithCheckbox';
 
+import { i18n } from 'web-translate';
+
 class ProctoringTerms extends Component {
     constructor(props) {
         super(props)
@@ -38,47 +40,41 @@ class ProctoringTerms extends Component {
         return (
             <div className={`shadow center${this.props.show ? '' : ' display-none'}`}>
 
-                <h2>ATTENTION: YOU WILL BE RECORDED</h2>
+                <h2>{i18n("ATTENTION: YOU WILL BE RECORDED")}</h2>
                 <div className="spacer-vertical-s"></div>
                 
                 <div className="width-adjust-1">
-                    <h4 className="" style={{color: '#222'}}>PLEASE READ CAREFULLY:</h4>
+                    <h4 className="" style={{color: '#222'}}>{i18n("PLEASE READ CAREFULLY:")}</h4>
                     <div className="spacer-vertical-s"></div>
-                    <p style={{color: '#444'}}>
-                    By clicking “begin” you acknowledge that you are aware that you will be recorded by Wise through 
-                    your device webcam, and the content of your screen will be recorded and displayed to 
-                    professors, in effort to offer a secure exam proctoring experience at educational institutions. 
-                    You hereby re-affirm that you have read, understand, and agree to our&nbsp; 
+                    <p style={{color: '#444'}}>{i18n("studentRecordAgreeToTerms_acknowledge")}&nbsp; 
                     <a 
                         href="https://www.wiseattend.com/privacy" 
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        privacy policy
-                    </a> and &nbsp; 
+                        {i18n("privacy policy")}
+                    </a> {i18n("and")} &nbsp; 
                     <a 
                         href="https://www.wiseattend.com/privacy" 
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        terms of service
+                        {i18n("terms of service")}
                     </a>. 
-                    Please know that all your information which is generated as part of using Wise is available for use
-                    by your school and by Wise Education Systems. Your information may be stored and transmitted at the discretion 
-                    of Wise to provide a secure exam proctoring experience.
+                    {i18n("studentRecordAgreeToTerms_information")}
                     </p>
                 </div>
                 <div className="spacer-vertical" />
-                {this.state.showMessage ? <p className="red center">"Please agree to the terms and conditions.</p> :''}
+                {this.state.showMessage ? <p className="red center">{i18n("Please agree to the terms and conditions.")}</p> :''}
                 
                 <TextWithCheckbox
-                    text="I agree to the terms of use."
+                    text={i18n("I agree to the terms of use.")}
                     checked={this.state.agreed}
                     onClick={this.handleClickAgree}
                 />
                 
                 <div className="spacer-vertical-s"></div>
-                <button onClick={this.handleConfirm} className="btn">Begin</button>
+                <button onClick={this.handleConfirm} className="btn">{i18n("Begin")}</button>
                 
             </div>
         )
