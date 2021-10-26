51f52958aca8db83415b361ef69b055a254f822d
Gregory Arnheiter <gmarnheiter@gmail.com>
2021-10-26 16:08:17 -0700

    Changed backend route for internal

--- src/store/axios.js
+++ src/store/axios.js
@@ -4,10 +4,10 @@ const axios = require('axios');
 // const baseURL = 'http://localhost:8080/';
 
 // URL testing internal site to work in China
-const baseURL = "https://wiseattendchina.com/"
+// const baseURL = "https://wiseattendchina.com/"
 
 // No Longer Used
-// const baseURL = 'https://internal-wiseattendonline.appspot.com/' // URL for hosted backend for test
+const baseURL = 'https://internal-wiseattendonline.appspot.com/' // URL for hosted backend for test
 
 /* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
 // const baseURL = 'https://wiseonlineattend.appspot.com/' // DO NOT USE! URL for hosted production.
